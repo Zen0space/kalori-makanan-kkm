3258cb1e74221018578733ef0e1b773217712212
Zen0space <kairul@zen0.space>
2025-07-17 20:50:06 +0800

    asset paths bug fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath } from "node:url";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/static/",
   resolve: {
     alias: {
       "@": resolve(dirname(fileURLToPath(import.meta.url)), "./src"),
